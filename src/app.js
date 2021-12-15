a40016262c059ec4c2710ea4423af4804a5b9188
Joao Victor <jov9051@outlook.com>
2021-12-15 15:29:20 -0300

    usersDb array created with nome, email, idade, pais and senha

--- src/app.js
+++ src/app.js
@@ -5,6 +5,16 @@ const { append } = require("express/lib/response");
 
 const PORT = 3000;
 
+let usersDb = [
+    {
+        nome: "Marco aurelio",
+        email: "teste123@email.com",
+        idade: "19",
+        pais: "Brasil",
+        senha: "fladkjl"
+    }
+];
+
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
