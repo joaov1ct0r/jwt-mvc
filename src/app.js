1fba7237a76f0d11ef0fcadc0c30bbecdf448b2b
Joao Victor <jov9051@outlook.com>
2021-12-15 15:25:34 -0300

    server running on port 3000

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
 const bodyParser = require("body-parser");
+const { append } = require("express/lib/response");
 
 const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
