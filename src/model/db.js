1ff238925820d0f61781ee1e01e1e49c5d5fd5c6
Joao Victor <jov9051@outlook.com>
2021-12-16 15:09:01 -0300

    function changeUser created inside db.js changing usersDb selected index data

--- src/model/db.js
+++ src/model/db.js
@@ -21,5 +21,17 @@ newUser (nome, email, idade, pais, senha){
 
 deleteUser(index){
   delete this.usersDb[index]
+},
+
+changeUser (index, nome, email, idade, pais, senha){
+    this.usersDb[index].nome = nome;
+
+    this.usersDb[index].email = email;
+
+    this.usersDb[index].idade = idade;
+
+    this.usersDb[index].pais = pais;
+
+    this.usersDb[index].senha = senha;
 }
 }
