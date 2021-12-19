53db494abb36395800eb76d7e5637425a5fa516f
Joao Victor <jov9051@outlook.com>
2021-12-19 10:43:20 -0300

    function for showing user info created

--- src/view/script/script.js
+++ src/view/script/script.js
@@ -101,42 +101,52 @@ infoLink.addEventListener("click", () => {
 });
 
 function showInfo() {
-    viewContainer.innerHTML = `
-        <div id="info-container">
-            <h1>Minhas Informações</h1>
+    let url = "http://localhost:3000/api/cadastros/all";
 
-            <br />
+    fetch(url)
+        .then(res => {
+            let data = res.json();
 
-            <label for="nome">Seu nome</label>
-            <br />
-            <input type="name" placeholder="Nome" name="nome" id="nome" />
+            return data;
+        })
+        .then(data => {
+            viewContainer.innerHTML = `
+                <div id="info-container">
+                    <h1>Minhas Informações</h1>
 
-            <br />
+                    <br />
 
-            <label for="email">Seu email</label>
-            <br />
-            <input type="email" placeholder="Email" name="email" id="email"
-            />
+                    <label for="nome">Seu nome</label>
+                    <br />
+                    <input type="name" placeholder="${data[0].nome}" name="nome" id="nome" disabled/>
 
-            <br />
+                    <br />
 
-            <label for="idade">Sua Idade</label>
-            <br />
-            <input type="number" placeholder="Idade" name="idade" id="idade"
-            />
+                    <label for="email">Seu email</label>
+                    <br />
+                    <input type="email" placeholder="${data[0].email}" name="email" id="email" disabled
+                    />
 
-            <br />
+                    <br />
 
-            <label for="pais">Seu país</label>
-            <br />
-            <input type="text" placeholder="País" name="pais" id="pais" />
+                    <label for="idade">Sua Idade</label>
+                    <br />
+                    <input type="number" placeholder="${data[0].idade}" name="idade" id="idade" disabled
+                    />
 
-            <br />
+                    <br />
 
-            <label for="senha">Sua senha</label>
-            <br />
-            <input type="password" placeholder="Senha" name="senha" id="senha" />
-        </div>`;
+                    <label for="pais">Seu país</label>
+                    <br />
+                    <input type="text" placeholder="${data[0].pais}" name="pais" id="pais" disabled />
+
+                    <br />
+
+                    <label for="senha">Sua senha</label>
+                    <br />
+                    <input type="password" placeholder="${data[0].senha}" name="senha" id="senha" disabled />
+                </div>`;
+        });
 }
 
 const submitCadastro = document.getElementById("submitCadastro");
