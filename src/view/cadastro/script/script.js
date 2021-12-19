04d86cc545ea01e9b998995bfdf66fcf06ef3743
Joao Victor <jov9051@outlook.com>
2021-12-19 12:01:24 -0300

    cadastro script.js file created inside cadastro folder

---
+++ src/view/cadastro/script/script.js
@@ -0,0 +1,41 @@
+const submitCadastro = document.getElementById("submitCadastro");
+
+submitCadastro.addEventListener("click", () => {
+    newUser();
+});
+
+function newUser() {
+    let nome = document.getElementById("nome").value;
+
+    let email = document.getElementById("email").value;
+
+    let idade = document.getElementById("idade").value;
+
+    let pais = document.getElementById("pais").value;
+
+    let senha = document.getElementById("senha").value;
+
+    let user = { nome, email, idade, pais, senha };
+
+    const url = "http://localhost:3000/api/new";
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    };
+
+    fetch(url, options).then(res => {
+        console.log(res);
+
+        document.getElementById("nome").value = "";
+
+        document.getElementById("email").value = "";
+
+        document.getElementById("idade").value = "";
+
+        document.getElementById("pais").value = "";
+
+        document.getElementById("senha").value = "";
+    });
+}
