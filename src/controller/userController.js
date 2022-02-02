78ad7713d67e59c52fd7964614ec5bd997de7c58
Joao Victor <jov9051@outlook.com>
2022-02-02 18:53:45 -0300

    codigo nao utilizado retirado de userController.js

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,11 +2,9 @@ const db = require('../model/db.js');
 
 let user = {
     info: function (req, res) {
-        let { email } = req.body;
+        let { email, senha } = req.body;
 
-        let { senha } = req.body;
-
-        let request = db.getUser(email, senha, function (result) {
+        db.getUser(email, senha, function (result) {
             res.send(JSON.stringify(result));
         });
     }
