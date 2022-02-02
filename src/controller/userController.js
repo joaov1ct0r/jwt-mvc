ffc1d6a8a142c7400b0fd134bb06b5f4e3951c91
Joao Victor <jov9051@outlook.com>
2022-02-02 18:51:45 -0300

    funcao info criada dentro de user em userController.js

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,3 +1,13 @@
 const db = require('../model/db.js');
 
-let user = {};
+let user = {
+    info: function (req, res) {
+        let { email } = req.body;
+
+        let { senha } = req.body;
+
+        let request = db.getUser(email, senha, function (result) {
+            res.send(JSON.stringify(result));
+        });
+    }
+};
