0124421ad1ebd90207837faf8de62c0b6f3184d4
Joao Victor <jov9051@outlook.com>
2022-02-03 19:42:47 -0300

    req.header em authController.js

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const jwtSecret = 'segredo';
 
 module.exports = (req, res, next) => {
-    const token = req.headers('auth-token');
+    const token = req.header('auth-token');
 
     if (!token) return res.status(401).send('Falha na autenticação');
 
