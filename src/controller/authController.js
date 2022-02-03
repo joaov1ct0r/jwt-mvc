9144044b63270cdc19b2df51b76699b0f954c10f
Joao Victor <jov9051@outlook.com>
2022-02-03 19:32:30 -0300

    condicional checando token criada

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,3 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {};
+module.exports = (req, res, next) => {
+    const token = req.headers('auth-token');
+
+    if (!token) return res.status(401).send('Falha na autenticação');
+};
