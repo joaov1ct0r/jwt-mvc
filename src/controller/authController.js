a237f5401ee6472a5bd8ddcdff6f0a4c67abbeab
Joao Victor <jov9051@outlook.com>
2022-02-03 19:33:50 -0300

    jwtSecret sendo importada de userController.js

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,7 +1,11 @@
 const jwt = require('jsonwebtoken');
 
+const { jwtSecret } = require('../controller/userController');
+
 module.exports = (req, res, next) => {
     const token = req.headers('auth-token');
 
     if (!token) return res.status(401).send('Falha na autenticação');
+
+    const userVerified = jwt.verify(token, jwtSecret);
 };
