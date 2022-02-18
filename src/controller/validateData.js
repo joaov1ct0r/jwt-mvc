f7605d38a1803011efec0468368eb3379ecafb77
Joao Victor <jov9051@outlook.com>
2022-02-18 20:51:37 -0300

    funcao registerValidate validando dados em validateData.js

--- src/controller/validateData.js
+++ src/controller/validateData.js
@@ -8,4 +8,6 @@ const registerValidate = data => {
         pais: Joi.string().required().min(2).max(30),
         senha: Joi.string().required().min(9).max(500)
     });
+
+    return schema.validate(data);
 };
