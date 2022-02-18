ab29acb6bb438a8e8cd1326a09a6abeacd3fae10
Joao Victor <jov9051@outlook.com>
2022-02-18 20:58:25 -0300

    funcao editValidate validando dados em validateData.js

--- src/controller/validateData.js
+++ src/controller/validateData.js
@@ -30,4 +30,6 @@ const editValidate = data => {
         pais: Joi.string().required().min(2).max(30),
         senha: Joi.string().required().min(9).max(500)
     });
+
+    return schema.validate(data);
 };
