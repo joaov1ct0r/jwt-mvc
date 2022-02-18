310162f880870fb31ead801c05e6af58bf202f9b
Joao Victor <jov9051@outlook.com>
2022-02-18 20:57:50 -0300

    var schema criada dentro da funcao editValidate em validateData.js

--- src/controller/validateData.js
+++ src/controller/validateData.js
@@ -21,4 +21,13 @@ const loginValidate = data => {
     return schema.validate(data);
 };
 
-const editValidate = data => {};
+const editValidate = data => {
+    let schema = Joi.object({
+        index: Joi.string().required(),
+        nome: Joi.string().required().min(3).max(50),
+        email: Joi.string().required().min(9).max(50),
+        idade: Joi.string().required().min(2).max(3),
+        pais: Joi.string().required().min(2).max(30),
+        senha: Joi.string().required().min(9).max(500)
+    });
+};
