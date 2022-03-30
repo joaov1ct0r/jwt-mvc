97fdd3c2d99268039c08ae655a622ba5de90be75
Joao Victor <jov9051@outlook.com>
2022-03-30 15:59:02 -0300

    feat: model definido em userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -2,6 +2,44 @@ import { DataTypes } from 'sequelize';
 
 import dbConnection from '../config/db.js';
 
+const User = dbConnection.define(
+    'usuarios',
+    {
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        idade: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        nome: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        pais: {
+            type: DataTypes.STRING(30),
+            allowNull: true,
+            defaultValue: 'Brasil'
+        },
+        senha: {
+            type: DataTypes.STRING(500),
+            allowNull: false
+        }
+    },
+    {
+        freezeTableName: true,
+        tableName: 'usuarios',
+        timestamps: false
+    }
+);
+
 const mysql = require('mysql2');
 
 let db = mysql.createPool({
